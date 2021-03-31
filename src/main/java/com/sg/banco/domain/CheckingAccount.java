59db4c6602cd163e91c6b51ca67666c4204137b2
Katia Marina Silva <ktiax13@gmail.com>
2021-03-31 18:21:59 -0300

    domain classes created - v1.0.1

--- src/main/java/com/sg/banco/domain/CheckingAccount.java
+++ src/main/java/com/sg/banco/domain/CheckingAccount.java
@@ -1,9 +1,29 @@
 package com.sg.banco.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@PrimaryKeyJoinColumn(name = "checking_account_id")
+@Entity
 public class CheckingAccount extends Account{
 
+    @Column
     private Double overdraftLimit;
+
+    @Column
     private Double interestRate;
+
+    @Column
     private Double interest;
 
 
--- src/main/java/com/sg/banco/domain/SavingsAccount.java
+++ src/main/java/com/sg/banco/domain/SavingsAccount.java
@@ -1,6 +1,22 @@
 package com.sg.banco.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@PrimaryKeyJoinColumn(name = "savings_account_id")
+@Entity
 public class SavingsAccount extends Account{
 
+    @Column
     private Double savings_income;
 }
