bda05ac0d73e62cad0acf2fb70debd41571db6b2
Katia Marina Silva <ktiax13@gmail.com>
2021-04-14 12:17:17 -0300

    unit test for deposit - v1.5

---
+++ src/test/java/com/sg/banco/service/TransactionServiceTest.java
@@ -0,0 +1,19 @@
+package com.sg.banco.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TransactionServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void createTransaction() {
+    }
+}
